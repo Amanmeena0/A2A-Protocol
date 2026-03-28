3693a857d3376be5fd0dc2c6f5cc9e92f2431633
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-03-28 13:24:25 +0530

    Started the MultiAgentfolder and import the libaries for the host agent from adk and a2a

---
+++ A2Amulit-agent/host_agent_adk.py/host/agent.py
@@ -0,0 +1,28 @@
+import asyncio
+import json
+import uuid 
+from datetime import datetime
+from typing import Any, AsyncIterable, List
+
+import httpx
+import nest_asyncio
+from a2a.client import A2ACardResolver
+from a2a.types import (
+    Agentcard,
+    MessageSendParams,
+    SendMessageRequest,
+    SendMessageResponse,
+    SendMessageSuccessResponse,
+    Task
+)
+
+from dotenv import load_dotenv
+from google.adk import Agent
+from google.adk.agents.readonly_context import ReadonlyContext
+from google.adk.artifacts import InMemoryArtifactService
+from google.adk.memory.in_memory_memory_service import InMemoryMemoryService
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+from google.adk.tools.tool_context import ToolContext
+from google.genai import types
+
